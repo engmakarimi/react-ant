83638154ab1c8bcb9e6d1b34b73ad71baa66fae9
engmakarimi <engineermakarimi@gmail.com>
2022-04-03 16:27:26 +0430

    initial

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+//import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import "../node_modules/antd/dist/antd.less";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = ReactDOM.createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
